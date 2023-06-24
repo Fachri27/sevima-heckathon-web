783e6cfc2428775b7321f521303dbaa83e7b76aa
fachri <mhmdfachri345@gmail.com>
2023-06-24 23:26:30 +0700

    *sevima-heckathon-web*

---
+++ db/kuis.sql
@@ -0,0 +1,101 @@
+LastSessions<|||>1<|||>project
+LastActiveSession<|||>1<|||>project
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+ColWidths_frmTableEditor.listColumns<|||>1<|||>42,100,156,90,60,65,50,119,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>10,100,80,120
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>23,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,408
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,298
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+MainWinWidth<|||>3<|||>993
+MainWinHeight<|||>3<|||>524
+SQLFile0<|||>1<|||>C:\Users\Lenovo\Documents\HeidiSQL_12.5_32_Portable\Backups\query-tab-2023-06-22_16-28-59-108.sql
+Servers\project\SessionCreated<|||>1<|||>2023-06-22 16:29:06
+Servers\project\Host<|||>1<|||>127.0.0.1
+Servers\project\WindowsAuth<|||>3<|||>0
+Servers\project\CleartextPluginEnabled<|||>3<|||>0
+Servers\project\User<|||>1<|||>root
+Servers\project\Password<|||>1<|||>3
+Servers\project\LoginPrompt<|||>3<|||>0
+Servers\project\Port<|||>1<|||>3306
+Servers\project\NetType<|||>3<|||>0
+Servers\project\Compressed<|||>3<|||>0
+Servers\project\LocalTimeZone<|||>3<|||>0
+Servers\project\QueryTimeout<|||>3<|||>30
+Servers\project\KeepAlive<|||>3<|||>20
+Servers\project\FullTableStatus<|||>3<|||>1
+Servers\project\Databases<|||>1<|||>
+Servers\project\Library<|||>1<|||>libmariadb.dll
+Servers\project\Comment<|||>1<|||>
+Servers\project\StartupScriptFilename<|||>1<|||>
+Servers\project\TreeBackground<|||>3<|||>536870911
+Servers\project\SSHtunnelActive<|||>3<|||>0
+Servers\project\SshExecutable<|||>1<|||>
+Servers\project\SSHtunnelHost<|||>1<|||>
+Servers\project\SSHtunnelHostPort<|||>3<|||>22
+Servers\project\SSHtunnelUser<|||>1<|||>
+Servers\project\SSHtunnelPassword<|||>1<|||>5
+Servers\project\SSHtunnelTimeout<|||>3<|||>4
+Servers\project\SSHtunnelPrivateKey<|||>1<|||>
+Servers\project\SSHtunnelPort<|||>3<|||>3307
+Servers\project\SSL_Active<|||>3<|||>0
+Servers\project\SSL_Key<|||>1<|||>
+Servers\project\SSL_Cert<|||>1<|||>
+Servers\project\SSL_CA<|||>1<|||>
+Servers\project\SSL_Cipher<|||>1<|||>
+Servers\project\IgnoreDatabasePattern<|||>1<|||>
+Servers\project\LogFileDdl<|||>3<|||>0
+Servers\project\LogFileDml<|||>3<|||>0
+Servers\project\LogFilePath<|||>1<|||>C:\Users\Lenovo\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\project\RefusedCount<|||>3<|||>3
+Servers\project\ServerVersionFull<|||>1<|||>5.7.24 - MySQL Community Server (GPL)
+Servers\project\ConnectCount<|||>3<|||>2
+Servers\project\ServerVersion<|||>3<|||>50724
+Servers\project\LastConnect<|||>1<|||>2023-06-24 17:26:40
+Servers\project\lastUsedDB<|||>1<|||>web_ai
+Servers\project\FavoriteObjects<|||>1<|||>db_blog||migrations<{{{><}}}>
+Servers\project\QueryHistory\1<|||>1<|||>2023-06-22 16:54:03|db_blog|0|SHOW DATABASES<{{{><}}}>
+Servers\project\QueryHistory\3<|||>1<|||>2023-06-22 16:54:25|db_blog|0|USE db_blog<{{{><}}}>
+Servers\project\QueryHistory\2<|||>1<|||>2023-06-24 17:28:49|web_ai|62|INSERT INTO users (nisn, nama, email, pass) VALUES('123', 'fff', '$ff@gmail.com', '123')<{{{><}}}>
+Servers\project\QueryHistory\5<|||>1<|||>2023-06-24 19:13:07|web_ai|47|INSERT INTO users (nisn, nama, email, pass) VALUES('$nisn', '$nama', '$email', '$pass')<{{{><}}}>
+Servers\project\QueryHistory\4<|||>1<|||>2023-06-24 19:13:49|web_ai|93|UPDATE users SET nisn = '$nisn', nama='$nama', email='$email', pass='$pass'
+Servers\project\QueryHistory\6<|||>1<|||>2023-06-24 21:52:54|web_ai|1451|CREATE TABLE quiz(<{{{><}}}>id INT PRIMARY KEY AUTO_INCREMENT,<{{{><}}}>soal TEXT,<{{{><}}}>pilihan ENUM ('a','b','c','d')<{{{><}}}>);<{{{><}}}>
+Servers\project\web_ai|users\MemoEditorHighlighter_nisn<|||>1<|||>
